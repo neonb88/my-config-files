84b67b8daec7fefe1e863d368352ff17c4369ae3
neonb88 <nathanbendich@gmail.com>
2018-06-20 12:33:35 -0700

    pltshow and blenderify

--- utils.py
+++ utils.py
@@ -3,6 +3,24 @@
 # TODO:   vim command "undo any"
 # TODO:   auto-updating ls, l, pwd, etc. commands (variable gets updated every 10 seconds within python shell and automatically pretty-prints)
 
+def blenderify(nparr):
+    '''
+        A more descriptive name would be "def make_list_of_tuples_from_nparr(arr):"
+        3-tuples contain (x, y, z) locations of the nonzero elements in arr
+        precondition: len(arr.shape)==3
+    '''
+    li=[]
+    for i in range(arr.shape[0]):
+        for j in range(arr.shape[1]):
+            for k in range(arr.shape[2]):
+                if(arr[i][j][k]):
+                    li.append((i,j,k))
+    return li
+
+
+def pltshow(im):
+    plt.imshow(im); plt.show(); plt.close()
+
 def newline(f):
     f.write('\n')
 
