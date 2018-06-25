4801bb3e85678b491a3bc60d1ce4549e1f49bebd
neonb88 <nathanbendich@gmail.com>
2018-06-24 21:26:04 -0700

    fixed stopwatch.py function

---
+++ stopwatch.py
@@ -0,0 +1,13 @@
+import time
+
+i           = 0
+step        = 1
+granularity = int(input("please input an integer step size for the timer"))
+while True:
+    if i % granularity == 0:
+        secs = i  % 60
+        mins = i // 60
+        print(str(mins)+" mins and "+str(secs)+" secs passed")
+    i += step
+    time.sleep (step)
+
