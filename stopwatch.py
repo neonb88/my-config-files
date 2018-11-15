84a8d241c6c00d79bdaa95da1da3858bb91f33fb
neonb88 <nathanbendich@gmail.com>
2018-11-15 18:00:00 -0500

    stopwatch.py: minor formatting fix

--- stopwatch.py
+++ stopwatch.py
@@ -2,12 +2,15 @@ import time
 
 i           = 0
 step        = 1
-granularity = int(input("please input an integer step size for the timer"))
+granularity = int(input("please input an integer step size for the timer:  "))
 while True:
     if i % granularity == 0:
         secs = i  % 60
         mins = i // 60
-        print(str(mins)+" mins and "+str(secs)+" secs passed")
+        if secs < 10:
+            print(str(mins)+" mins and "+str(secs)+"  secs passed")
+        else:
+            print(str(mins)+" mins and "+str(secs)+ " secs passed")
     i += step
     time.sleep (step)
 
