834c0da56aeefc6ddb97797aadb54263107507cb
neonb88 <nathanbendich@gmail.com>
2019-05-15 21:08:57 -0400

    GPA_calculation.py;  Columbia Computer Science GPA is ~3.22

---
+++ pyutils/GPA_calculation.py
@@ -0,0 +1,113 @@
+
+with open('/home/n/GPA_calculation__python_input.txt', 'r') as fp:
+  lines=fp.readlines()
+lines=lines[1:]
+tot=0
+tot_pts=0
+
+for line in lines:
+  pts,GPA=line.split('  ')
+  pts=float(pts)
+  GPA=float(GPA)
+  tot_pts+=pts
+  tot+=pts*GPA
+
+tot /=tot_pts
+print("tot: ", tot) # finally,     tot: 3.2236111111111114
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
