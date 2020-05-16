2b1ec473fb238c467bd9e6501ab69101226fa89a
neonb88 <nathanbendich@gmail.com>
2020-05-16 01:52:57 -0400

    touch_symlinks.sh :   on Ubuntu_Laptop,   'refreshes' the files that I want to see at the bottom of output from 'l'

---
+++ touch_symlinks.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+#=========================================================================================================
+
+# update the symblinks:
+touch -h  /home/n/obj_2_png
+touch -h  /home/n/V
+touch -h  /home/n/OpenPose_json
+touch -h  /home/n/test_data.pkl_contents 
+touch -h  /home/n/x/p/fresh____as_of_Dec_12_2018/vr_mall____fresh___Dec_12_2018/openpose_json_cmd.sh
+touch -h  /home/n/how_2_run.txt
+touch -h  w8_4_img_upload_____then_run_pyrender__.py
+touch -h  /home/n/Di_instructions_April_2_2020
+touch -h  /home/n/save_smpl_obj_file.py
+touch -h  /home/n/SMPL_sandbox.py
+touch -h  /home/n/Multi-Garment_dataset
+
+# temporary symlinks:
+touch -h  /home/n/Documents/papers/grep.sh
+touch -h  /home/n/to_mayor_of_Newark_DE_Jerry_Clifton_____COVID-19.txt
+
+
+#=========================================================================================================
+#=========================================================================================================
+#=========================================================================================================
+
+
+# actual folders / files  :
+touch     /home/n/obj_2_png
+touch     /home/n/V
+touch     /home/n/touch_symlinks.sh
+touch     /home/n/cs231n_spring_2019
+touch     /home/n/intermediate_steps_ClothX/
+touch     /home/n/OpenPose_json
+touch     /home/n/Downloads/test_data.pkl_contents
+touch     /home/n/x/p/fresh____as_of_Dec_12_2018/vr_mall____fresh___Dec_12_2018/ref/openpose/openpose_json_cmd.sh
+touch     /home/n/MultiGarmnentNetwork/README.md
+touch     /home/n/x/p/fresh____as_of_Dec_12_2018/vr_mall____fresh___Dec_12_2018/smplx/Blender_obj_mesh_render_____interaction/w8_4_img_upload_____then_run_pyrender__.py
+touch     /home/n/Downloads/Di_instructions_April_2_2020
+touch     /home/n/x/p/fresh____as_of_Dec_12_2018/vr_mall____fresh___Dec_12_2018/smpl/smpl_webuser/hello_world/save_smpl_obj_file.py
+touch     /home/n/BLB
+touch     /home/n/BLB/MultiGarmentNetwork/python3_port_to_BLB/SMPL_sandbox.py
+touch     /home/n/Downloads/Multi-Garment_dataset
+touch     /home/n/2nd.txt
+touch     /home/n/3rd.txt
+
+
+# temporary:
+touch     /home/n/.vim_session_measure_py_____April_24_2020_____debugging_measure_inseam
+touch     /home/n/.vim_session_____test_network_MGN_____April_24_2020
+touch     /home/n/Documents/papers/grep.sh
+
+#=========================================================================================================
+
+
+
