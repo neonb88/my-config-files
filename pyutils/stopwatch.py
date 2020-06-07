0ddcfdad1e75b437b2828b9af59c5d9887291c21
neonb88 <nathanbendich@gmail.com>
2020-06-07 12:09:30 -0400

    stopwatch.py   : added 'set initial time' feature.

--- pyutils/stopwatch.py
+++ pyutils/stopwatch.py
@@ -1,22 +1,25 @@
 import sys
 import time
 
-
 i           = 0
-step        = 1
+step        = 1 # NOTE:  why is step = 1?   Should it be   "`STEP`"  for a constant?
+
+
 if len(sys.argv) == 1:
   granularity = int(input("please input an integer step size for the timer:  "))
-else:
+elif len(sys.argv) == 2:
   granularity = int(sys.argv[1])
+elif len(sys.argv) == 3:
+  granularity = int(sys.argv[1])
+  i           = int(sys.argv[2]) 
+
+
 
 while True:
-    if i % granularity == 0:
-        secs = i  % 60
-        mins = i // 60
-        if secs < 10:
-            print(str(mins)+" mins and  "+str(secs)+" secs passed")
-        else:
-            print(str(mins)+" mins and " +str(secs)+" secs passed")
-    i += step
-    time.sleep (step)
+  if i % granularity == 0:
+    secs = i  % 60
+    mins = i // 60
+    print(str(mins)+" mins and  "+str(secs)+" secs passed")
+  i += step
+  time.sleep (step)
 
